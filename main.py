ac570b47d82ce93ff4446bf6421598b124068970
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2025-03-18 12:47:02 -0700

    Improved traceback and found (most?) brown mnms

--- main.py
+++ main.py
@@ -2,46 +2,55 @@ import random
 import numpy as np
 import math
 
-data = np.array([
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5]
-])
-
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    return random.uniform(0, 100)
+    # features we care about are label, current_set, and feature_to_add
+    feature_indices = [0] + current_set + [feature_to_add]
+    subsetData = data[:, feature_indices]
+    subsetAccuracy = accuracy(subsetData)
+    print(f"\tUsing feature(s) {current_set + [feature_to_add]} accuracy is {subsetAccuracy:.1f}%")
+    return subsetAccuracy
 
 def forward_feature_search(data):
+    print_data_info(data)
 
     current_set_of_features = [] # initialize an empty set
 
-    # from column 2 (features) to end
+    best_overall_feature_set = []
+    best_overall_accuracy = 0
+
+    # from column 2 (start of features) to end
     for i in range(1, data.shape[1]):
-        print(f"On the {i}th level of the search tree")
+        #print(f"On the {i}th level of the search tree")
         feature_to_add_at_this_level = None
         best_so_far_accuracy = 0
 
         # from column 2 to end
         for k in range(1, data.shape[1]):
             if (current_set_of_features.__contains__(k) == False):  # only add unconsidered features
-                print(f"--Considering adding the {k} feature")
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k + 1)
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
 
                 if (accuracy > best_so_far_accuracy):   # found a better accuracy feature
                     best_so_far_accuracy = accuracy
                     feature_to_add_at_this_level = k
         
         current_set_of_features.append(feature_to_add_at_this_level)
-        print(f"On level {i} i added feature {feature_to_add_at_this_level} to current set")
+
+        print("")
+        if (best_so_far_accuracy < best_overall_accuracy):
+            print(f"(Warning, Accuracy has decreased! Continuing search in case of local maxima)")
+        print(f"Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy:.1f}%\n")
+        # check if its our new overall best
+        if (best_so_far_accuracy > best_overall_accuracy):
+            best_overall_accuracy = best_so_far_accuracy
+            best_overall_feature_set = current_set_of_features.copy()
+    
+    print(f"Finished search!! The best feature subset is {best_overall_feature_set}, which has an accuracy of {best_overall_accuracy:.1f}%\n")
 
 # TODO: implement backward feature search
 def backward_feature_search(data):
     return None
 
-def accuracy():
-    # load the data from filepath
-    file_path = r"D:\Program Files\GitHub\CS170-P2-NearestNeighbor\CS170_Small_Data__12.txt"
-    data = np.loadtxt(file_path)
+def accuracy(data):
     number_correctly_classified = 0
 
     # for all rows (data points)
@@ -68,7 +77,14 @@ def accuracy():
         
     # accuracy = # correct / # data points
     accuracy = number_correctly_classified / data.shape[0]
-    return accuracy
+    return accuracy * 100
+
+def print_data_info(data):
+    rows, cols = data.shape
+    print(f"This dataset has {cols - 1} features (not including the class attribute), with {rows} instances.\n")
+    allAccuracy = accuracy(data)
+    print(f"Running nearest neighbor with all {cols - 1} features, using \"leaving-one-out\" evaluation, I get an accuracy of {allAccuracy:.1f}%\n")
+
 
 def select_data():
     testFile = input(f"Type in the name of the file to test : ")
@@ -79,19 +95,20 @@ def select_data():
 
     except FileNotFoundError:
         print(f"Error: The file '{testFile}' was not found. Please check the file name and try again.")
-        select_data()
+        return select_data()
     except ValueError:
         print(f"Error: The data in '{testFile}' has an incorrect format. Please check the file.")
-        select_data()
+        return select_data()
     except Exception as e:
         print(f"An unexpected error occurred while loading data: {e}")
-        select_data()
+        return select_data()
     
-def select_algorithm():
+def select_algorithm(data):
     print(f"Type the number of the algorithm you want to run.")
     print(f"\t1) Forward Selection")
     print(f"\t2) Backward Elimination")
     algorithm = input()
+    print("")
 
     if (algorithm == "1"):
         forward_feature_search(data)
@@ -99,15 +116,15 @@ def select_algorithm():
         backward_feature_search(data)
     else:
         print(f"Unrecognized algorithm entered")
-        select_algorithm()
+        select_algorithm(data)
 
 def main():
     print(f"Welcome to Niko Udria's Feature Selection Algorithm.")
     # comments for easy filename copy paste
     # CS170_Large_Data__62.txt
     # CS170_Small_Data__12.txt
-    select_data()
-    select_algorithm()
+    data = select_data()
+    select_algorithm(data)
 
 if __name__ == "__main__":
     main()
